43ab36edd032e416c51a28465eac00efa7703452
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-12-22 20:20:46 -0300

    [R1] Initializing angular project

---
+++ customer-contact-list-web/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CustomerContactFormComponent } from './customer-contact-form/customer-contact-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerContactFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

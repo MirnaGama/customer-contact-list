a098f3522a2f26acddc03bb6280d9be3e84b0055
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-12-23 18:38:25 -0300

    [R1] Set customer contact form as default route for now

--- customer-contact-list-web/src/app/app-routing.module.ts
+++ customer-contact-list-web/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerContactFormComponent } from './customer-contact-form/customer-contact-form.component';
 
-const routes: Routes = [ { path: 'customer-contact-form', component: CustomerContactFormComponent }];
+const routes: Routes = [ { path: '', component: CustomerContactFormComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

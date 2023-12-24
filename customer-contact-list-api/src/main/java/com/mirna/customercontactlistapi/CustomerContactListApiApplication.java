29451a70eaca1c4bc18737e24c45b85d6105eaa1
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-12-24 16:15:23 -0300

    [R2] Spring Initializr

---
+++ customer-contact-list-api/src/main/java/com/mirna/customercontactlistapi/CustomerContactListApiApplication.java
@@ -0,0 +1,13 @@
+package com.mirna.customercontactlistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerContactListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerContactListApiApplication.class, args);
+	}
+
+}

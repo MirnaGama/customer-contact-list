431e5a9e661c94e36113269702be2784775ca772
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-12-24 01:50:13 -0300

    [R1] Add pattern validator to check if full name has only letters

--- customer-contact-list-web/src/app/customer-contact/components/customer-contact-form/customer-contact-form.component.ts
+++ customer-contact-list-web/src/app/customer-contact/components/customer-contact-form/customer-contact-form.component.ts
@@ -31,7 +31,7 @@ export class CustomerContactFormComponent {
   ngOnInit(): void {
     this.customerContactForm = this.formBuilder.group(
       {
-        fullname: ['', Validators.compose([Validators.required, Validators.minLength(10)])],
+        fullname: ['', Validators.compose([Validators.required, Validators.minLength(10), Validators.pattern("/^[^a-zA-Z]*$/")])],
         cep: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
         street: ['', [Validators.required]],
         houseNumber: ['', [Validators.required]],

df9db86d020adf5e214d030e22e3d269d5a12535
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-12-24 16:15:23 -0300

    [R2] Spring Initializr

---
+++ customer-contact-list-api/src/test/java/com/mirna/customercontactlistapi/CustomerContactListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirna.customercontactlistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerContactListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

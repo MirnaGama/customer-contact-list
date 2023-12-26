467320856ca9031a1d1635e69cbf01c3a5609911
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-12-25 21:21:24 -0300

    [R2] Add entity not present exception

---
+++ customer-contact-list-api/src/main/java/com/mirna/customercontactlistapi/exceptions/EntityNotPresentException.java
@@ -0,0 +1,8 @@
+package com.mirna.customercontactlistapi.exceptions;
+
+public class EntityNotPresentException extends Exception {
+
+	public EntityNotPresentException(String message) {
+		super(message);
+	}
+}

dad1128ca64c647889bf30f464847ec7ed3cd106
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-12-26 02:36:43 -0300

    [R2] Add Java Doc in validation class for telephone number

--- customer-contact-list-api/src/main/java/com/mirna/customercontactlistapi/domain/entities/validation/TelephoneNumberRegex.java
+++ customer-contact-list-api/src/main/java/com/mirna/customercontactlistapi/domain/entities/validation/TelephoneNumberRegex.java
@@ -1,6 +1,11 @@
 package com.mirna.customercontactlistapi.domain.entities.validation;
 
-
+/**
+ * This class has the value of the regex used to check for the valid format of the phone number
+ * 
+ * @author Mirna Gama
+ * @version 1.0
+ */
 public class TelephoneNumberRegex {
 
 	public static final String VALUE = "^1\\d\\d(\\d\\d)?$|^0800 ?\\d{3} ?\\d{4}$|^(\\(0?([1-9a-zA-Z][0-9a-zA-Z])?[1-9]\\d\\) ?|0?([1-9a-zA-Z][0-9a-zA-Z])?[1-9]\\d[ .-]?)?(9|9[ .-])?[2-9]\\d{3}[ .-]?\\d{4}$";

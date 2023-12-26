883f92ebbe7d94a949fb7e8b19fcbdf50bf200e8
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-12-26 02:10:17 -0300

    [R2] Add Java Doc in repositories

--- customer-contact-list-api/src/main/java/com/mirna/customercontactlistapi/repositories/CustomerRepository.java
+++ customer-contact-list-api/src/main/java/com/mirna/customercontactlistapi/repositories/CustomerRepository.java
@@ -7,10 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.mirna.customercontactlistapi.domain.entities.Customer;
 
 /**
- * 
+ * Repository interface for retrieving and manipulating all Customer objects using their unique UUID identifier.
+*
  * @author Mirna Gama
- * @version 1.0
- */
+* @version 1.0
+*/
 public interface CustomerRepository extends JpaRepository<Customer, UUID>{
 
 }

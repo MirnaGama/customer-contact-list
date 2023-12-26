f7bfd68b45ba6889aa97bbe10137656d0d161202
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-12-26 02:10:37 -0300

    [R2] Add Java Doc in mappers

--- customer-contact-list-api/src/main/java/com/mirna/customercontactlistapi/domain/mappers/CustomerMapper.java
+++ customer-contact-list-api/src/main/java/com/mirna/customercontactlistapi/domain/mappers/CustomerMapper.java
@@ -7,6 +7,9 @@ import com.mirna.customercontactlistapi.domain.dto.CustomerDTO;
 import com.mirna.customercontactlistapi.domain.entities.Customer;
 
 /**
+ * This mapper provides functionality for converting between Customer entity
+ * class and matching data transfer objects using
+ * com.fasterxml.jackson.databind.ObjectMapper
  * 
  * @author Mirna Gama
  * @version 1.0
@@ -14,17 +17,29 @@ import com.mirna.customercontactlistapi.domain.entities.Customer;
 @Component
 public class CustomerMapper {
 
-	 	private ModelMapper mapper;
-	 
-	    public CustomerMapper() {
-	    	mapper =  new ModelMapper();
-	    }
-	    
-	    public Customer toCustomerEntity(CustomerDTO customerDTO) {
-	        return mapper.map(customerDTO, Customer.class);
-	    }
+	private ModelMapper mapper;
 
-	    public CustomerDTO toUserDTO(Customer customerEntity) {
-	        return mapper.map(customerEntity, CustomerDTO.class);
-	    }
+	public CustomerMapper() {
+		mapper = new ModelMapper();
+	}
+
+	/**
+	 * Returns a entity object of type Customer
+	 * 
+	 * @param customerDTO Data transfer object that will be converted
+	 * @return Mapped customer entity
+	 */
+	public Customer toCustomerEntity(CustomerDTO customerDTO) {
+		return mapper.map(customerDTO, Customer.class);
+	}
+
+	/**
+	 * Returns a data transfer object containing Customer entity data
+	 * 
+	 * @param customerEntity Entity object that will be converted
+	 * @return Mapped customer data transfer object
+	 */
+	public CustomerDTO toUserDTO(Customer customerEntity) {
+		return mapper.map(customerEntity, CustomerDTO.class);
+	}
 }
